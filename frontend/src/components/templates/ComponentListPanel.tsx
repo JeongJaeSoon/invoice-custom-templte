fecbf3476ef91de746459a4c43d82262fb41b5a4
JeongJaeSoon <94jaesoon.jeong@gmail.com>
2025-02-11 15:21:56 +0900

    feat: Remove unused useEffect import from ComponentListPanel

--- frontend/src/components/templates/ComponentListPanel.tsx
+++ frontend/src/components/templates/ComponentListPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { CanvasComponent } from '../../types/CanvasComponent';
